5d3038d43ffcc8483f94bd381eb57b8b59b799a8
AbdelrahmanRagab38 <abdelrahmanRagab38@gmail.com>
2020-03-19 23:35:47 -1000

    first commit with the project folders

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bookangy',
+        theme: ThemeData(
+        primarySwatch: Colors.green,
+        scaffoldBackgroundColor: Color(0XFFCCCCCC)),
+    home: SplashScreen(),
+    routes: {
+    '/home': (BuildContext context) => HomePage(),
+    '/login': (BuildContext context) => LoginPage(),
+    '/signup': (BuildContext context) => SignupPage(),
+    '/about': (BuildContext context) => AboutPage(),
+    },
+    );
+}
+
+

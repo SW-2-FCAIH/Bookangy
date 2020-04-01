6a703eef0d41771f4390514b3bb40dc045c7f358
Asmaa_Nasser <asmaa98seif@gmail.com>
2020-04-01 21:28:18 +0200

    changing the application name to version 1

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bookangy',
+      title: 'Bookangy_one',
         theme: ThemeData(
         primarySwatch: Colors.green,
         scaffoldBackgroundColor: Color(0XFFCCCCCC)),

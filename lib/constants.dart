afd116799c6d4f6160d769d6bd45e062ee4dfd36
Asmaa_Nasser <asmaa98seif@gmail.com>
2020-05-30 14:14:35 +0200

    the constants

---
+++ lib/constants.dart
@@ -0,0 +1,67 @@
+
+import 'package:flutter/material.dart';
+
+
+const kSendButtonTextStyle = TextStyle(
+  color: Colors.lightBlueAccent,
+  fontWeight: FontWeight.bold,
+  fontSize: 18.0,
+);
+
+const kMessageTextFieldDecoration = InputDecoration(
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  hintText: 'Type your message here...',
+  border: InputBorder.none,
+);
+
+const kMessageContainerDecoration = BoxDecoration(
+  border: Border(
+    top: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+  ),
+);
+
+const kInputDecoration = InputDecoration(
+  hintText: 'Enter your value.',
+  contentPadding:
+  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide:
+    BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide:
+    BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+);
+
+const kMainColor = Color(0xFFFFC12F);
+const kSecondaryColor = Color(0xFFFFE6AC);
+
+const kUnActiveColor = Color(0xFFC1BDB8);
+
+
+
+const kProductName = 'bookTitle';
+const kProductNpages = 'numPages';
+const kProductprice = 'price';
+const kProductstars = 'stars';
+const kProductDescription = 'description';
+const kProducturl = 'imageUrl';
+const kProductCategory = 'authorName';
+const kProductsCollection = 'Books';
+
+
+const kUserEmail = 'email';
+const kUserPassword= 'password';
+const kUserName= 'username';
+const kUserId= 'uid';
+
+const kJackets = 'jackets';
+const kTrousers = 'trousers';
+const kTshirts = 't-shirts';
+const kShoes = 'shoes';
